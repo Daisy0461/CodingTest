0b23627815c1bb7c8747752235db92ccb5f78688
jaejune Yun <jjune0427@naver.com>
2024-10-19 01:03:55 +0900

    백준 2529 풀이

--- Test.cpp
+++ Test.cpp
@@ -1,54 +1,86 @@
-//1987
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int r, c, result = 0;
-int alphaArray[25][25];
-int visitedAlpha[50];
-int dy[] = { 0, 1, 0, -1 };
-int dx[] = { 1, 0 , -1, 0 };
+bool bIsFindResult = false;
+int bodingNum;
+int usedNum[10];
+vector<int> result;
+vector<bool> vIsFindBigger;
 
-void dfs(int y, int x, int count)
+void FindResult(int num, int index)
 {
-	visitedAlpha[alphaArray[y][x]] = 1;		//지금 들어온 알파벳을 방문처라한다.
+	usedNum[num] = 1;
+	if (index == bodingNum && bIsFindResult == false) {
+		//cout << "Find Result \n";
+		for (int i : result) {
+			cout << i;
+		}
+		cout << "\n";
 
-	bool haveNext = false;
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;		//범위 밖이면 continue;
-		if (visitedAlpha[alphaArray[ny][nx]]) continue;		//이미 방문한 것이라면 continue;
-
-		haveNext = true;		//다음으로 나아갈 칸이 아직 남아있다.
-		dfs(ny, nx, count + 1);
+		bIsFindResult = true;
+		return;
 	}
 
-	if (!haveNext) {							//현재 칸에서 더 나아가지 못해서 max와 비교한다.
-		result = max(count, result);
+	if (bIsFindResult) return;
+
+	if (vIsFindBigger[index]) {		//지금 num보다 더 큰 값을 찾는다.
+		for (int i = num + 1; i < 10; i++) {
+			if (usedNum[i]) continue;
+
+			result.push_back(i);
+			FindResult(i, index + 1);
+			result.pop_back();
+		}
+	}
+	else {
+		for (int i = num - 1; i >= 0; i--) {
+			if (usedNum[i]) continue;
+
+			result.push_back(i);
+			FindResult(i, index + 1);
+			result.pop_back();
+		}
 	}
 
-	visitedAlpha[alphaArray[y][x]] = 0;		//다른 곳으로 갈 경우에 현재 칸에 해당하는 alpha가 나올 수 있으므로 0으로 다시 만들어준다.
+	usedNum[num] = 0;
 }
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
-	cout.tie(0); cin.tie(0);
+	cin.tie(0); cout.tie(0);
 
-	cin >> r >> c;
-	
+	cin >> bodingNum;
+
 	char temp;
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			cin >> temp;
-			alphaArray[i][j] = temp - 'A';
+	for (int i = 0; i < bodingNum; i++) {
+		cin >> temp;
+		if (temp == '<') {
+			vIsFindBigger.push_back(true);
+		}
+		else if (temp == '>') {
+			vIsFindBigger.push_back(false);
 		}
 	}
 
-	dfs(0, 0, 1);
+	for (int i = 9; i >= 0; i--) {
+		result.push_back(i);
+		FindResult(i, 0);
+		result.pop_back();
 
-	cout << result;
+		if (bIsFindResult) break;
+	}
+
+	result.clear();
+	bIsFindResult = false;
+	fill(&usedNum[0], &usedNum[0] + 10, 0);
+
+	for (int i = 0; i < 10; i++) {
+		result.push_back(i);
+		FindResult(i, 0);
+		result.pop_back();
+
+		if (bIsFindResult) break;
+	}
 }
