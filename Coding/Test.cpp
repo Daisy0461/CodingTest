062502466ad70ae7c7cc9fa6159a906c446d5309
Jaejune <jjune0427@naver.com>
2024-10-24 02:24:58 +0900

    14890 풀이

--- Coding/Test.cpp
+++ Coding/Test.cpp
@@ -2,96 +2,156 @@
 
 using namespace std;
 
-int n, minResult = 987654321;
-int tempIndex, tempSum;
-int visited[20];
-vector<int> populars;
-vector<int> Sums;
-vector<vector<int>> Links(20);
-
-void FindLink(int vilage, bool isOn)
-{
-	visited[vilage] = 1;
-	tempSum += populars[vilage];
-
-	for (int i : Links[vilage]) {
-		//cout << "In i:  " << i << "\n";
-		if (visited[i]) continue;
-
-		//if((temp & (1<<i)) == 1)로 하면 안되네.. 이게 1이 아니라 2든 4든 8이든 될 수가 있어서 안됐구나
-
-		if (isOn && (tempIndex & (1 << i))) {
-			//cout << "On Find Link Vilage : " << i << "\n";
-			FindLink(i, isOn);
-		}
-		else if (!isOn && !(tempIndex & (1 << i))) {
-			//cout << "Off Find Link Vilage : " << i << "\n";
-			FindLink(i, isOn);
-		}
-	}
-}
+int n, l, result = 0;		//l은 경사로의 가로 길이, 경사로의 높이는 항상 1
+vector<vector<int>> v(101);
+vector<vector<int>> vr(101);
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n;
+	cin >> n >> l;
 
-	int pop;
-	for (int i = 0; i < n; i++) {				//인구 수 입력
-		cin >> pop;
-		populars.push_back(pop);
-	}
-
-	int linkNum;
-	for (int i = 0; i < n; i++) {						//link확인 & 잘들어가는 것 확인 완료
-		cin >> linkNum;
-		int link;
-		for (int j = 0; j < linkNum; j++) {
-			cin >> link;
-			Links[i].push_back(link - 1);			//link - 1을 해줘야한다. 만약 link가 1이면 0번째 비트와 비교해서 계산할 것이기 때문이다.
-		}
-	}
-
-	for (int i = 1; i < ((1 << n) - 1); i++) {
-		fill(&visited[0], &visited[0] + 20, 0);
-		Sums.clear();
-		tempIndex = i; tempSum = 0;
-		//cout << "i : " << tempIndex << "\n";
-		
+	int height;
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (visited[j]) continue;
+			cin >> height;
+			v[i].push_back(height);
+			vr[j].push_back(height);
+		}
+	}
 
-			tempSum = 0;
-			if ((tempIndex & (1 << j)) == 0) {		//비트가 꺼져있다면
-				//cout << "Bit Off Start Vilage : " << j << "\n";
-				FindLink(j, false);
-				Sums.push_back(tempSum);
+	//cout << "Reverse Print\n";
+	//for (int i = 0; i < n; i++) {
+	//	for (int j : vr[i]) {
+	//		cout << j << " ";
+	//	}
+	//	cout << "\n";
+	//}
+
+	for (int i = 0; i < n; i++) {
+		int sameCount = 0;
+		int beforeHeight = 0;		//0이면 처음이다. L이 1부터 시작이기에
+		int j = 0;
+		//cout << "i : " << i;
+		for (j = 0; j < n; j++) {
+			int currentHeight = v[i][j];
+
+			if (beforeHeight == 0) {		//처음 넣어주기
+				sameCount++;
+				beforeHeight = currentHeight;
+				continue;
 			}
-			else {
-				//cout << "Bit On Start Vilage : " << j << "\n";
-				FindLink(j, true);
-				Sums.push_back(tempSum);
+
+			if (beforeHeight == v[i][j]) {		//현재 높이와 같다면
+
+				sameCount++;
+			}
+			else {		//beforeHeight != j
+				int subResult = beforeHeight - currentHeight;
+				if (abs(subResult) > 1) break;	//차이가 1보다 크면 이미 경사로 못 놓음.
+					
+
+				if (subResult == -1) {		//즉 지금 높이가 이전보다 1 높다.
+					if (sameCount < l) break;	//sameCount가 경사로의 가로보다 작으면 경사로 못 놓음.
+					sameCount = 1;
+					beforeHeight = currentHeight;
+				}
+				else if (subResult == 1) {		//지금 높이가 이전보다 1낮다.
+					if (j + l - 1 >= n) {	//n보다 크면 못놓는다. 넘어간다.
+						j = n + 1;
+						continue;
+					}
+
+					bool can = true;
+					for (int k = 1; k <= l - 1; k++) {
+						if (v[i][j + k] == currentHeight) {
+							continue;
+						}
+						else {
+							can = false;
+						}
+					}
+
+					if (!can) break;
+
+					j = j + l - 1;
+					sameCount = 0;			//초기로 돌린다.
+					beforeHeight = currentHeight;
+				}
 			}
 		}
 
-		if (Sums.size() == 2) {
-			int tempSumsResult = abs(Sums[0] - Sums[1]);
-			//cout << "Sums[0] : " << Sums[0] << "  Sums[1] : " << Sums[1] << "\n";
-			//cout << "Sum Sub Result : " << tempSumsResult;
-		
-			minResult = min(minResult, tempSumsResult);
+		if (j == n) {
+			//cout << " is result ++ \n";
+			result++;
 		}
 		else {
-			//cout << "Sums Size : " << Sums.size();
+			//cout << "\n";
 		}
-		//cout << "\n";
 	}
 
-	if (minResult == 987654321) {
-		cout << -1;
-	}else{
-		cout << minResult;
+	//cout << "Reverse \n";
+	for (int i = 0; i < n; i++) {
+		//cout << "i : " << i;
+		int sameCount = 0;
+		int beforeHeight = 0;		//0이면 처음이다. L이 1부터 시작이기에
+		int j = 0;
+		for (j = 0; j < n; j++) {
+			int currentHeight = vr[i][j];
+
+			if (beforeHeight == 0) {		//처음 넣어주기
+				sameCount++;
+				beforeHeight = currentHeight;
+				continue;
+			}
+
+			if (beforeHeight == vr[i][j]) {		//현재 높이와 같다면
+				sameCount++;
+			}
+			else {		//beforeHeight != j
+				int subResult = beforeHeight - currentHeight;
+				if (abs(subResult) > 1) break;	//차이가 1보다 크면 이미 경사로 못 놓음.
+
+
+				if (subResult == -1) {		//즉 지금 높이가 이전보다 1 높다.
+					if (sameCount < l) break;	//sameCount가 경사로의 가로보다 작으면 경사로 못 놓음.
+					sameCount = 1;
+					beforeHeight = currentHeight;
+				}
+				else if (subResult == 1) {		//지금 높이가 이전보다 1낮다.
+					if (j + l - 1 >= n) break;		//n보다 크면 못놓는다. 넘어간다.
+
+					bool can = true;
+					for (int k = 1; k <= l - 1; k++) {
+						if (vr[i][j + k] == currentHeight) {
+							continue;
+						}
+						else {
+							can = false;
+						}
+					}
+
+					if (!can) break;
+
+					j = j + l - 1;
+					sameCount = 0;			//초기로 돌린다.
+					beforeHeight = currentHeight;
+				}
+			}
+		}
+
+		if (j == n) {
+			//cout << " is result ++ \n";
+			result++;
+		}
+		else {
+			//cout << "\n";
+		}
 	}
+
+	cout << result;
+
+
 }
