ec1a701676ce46da2f317ea85e411a8f818e32b5
Jaejune <jjune0427@naver.com>
2024-10-30 22:59:44 +0900

    2109완료

--- Coding/Test.cpp
+++ Coding/Test.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-int n;	//학생 수
-priority_queue<float, vector<float>, greater<float>> pq;
+int n, result = 0;
+priority_queue<pair<int, int>> pq;		//date, pay
+priority_queue<int> pq2;
 
 int main()
 {
@@ -12,14 +13,32 @@ int main()
 
 	cin >> n;
 
-	float score;
+	//pay와 date 순으로 주어진다.
+	int p, d;
 	for (int i = 0; i < n; i++) {
-		cin >> score;
-		pq.push(score);
+		cin >> p >> d;			//pay가 큰순으로 정리하고 day에 맞춰야한다.
+		pq.push({ d, p });		//d가 큰순으로 정렬된다.
 	}
 
-	for (int i = 0; i < 5; i++) {
-		cout << pq.top() << "\n";
-		pq.pop();
+	//day가 작은 순으로 넣어야할 것 같은데.
+	//pay가  큰순으로 정렬하고 뒤에 해도 되는 것(date가 큰 것)을 pq에 넣으면 그 안에 해도 되니까 그런 식으로 진행해보자.
+
+	int highDate = 0;
+	if (!pq.empty()) {
+		highDate = pq.top().first;
 	}
+	for (int i = highDate; i > 0; i--) {
+		while (!pq.empty() && pq.top().first == i) {
+			pq2.push(pq.top().second);			//pay가 큰 순으로 정렬된다.
+			pq.pop();
+		}
+
+		if (!pq2.empty()) {
+			result += pq2.top();
+			pq2.pop();
+		}
+	}
+
+	cout << result;
+
 }
