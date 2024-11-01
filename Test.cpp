76faf2a57a179ec7b9efea9cdb560c757cb8a8fe
jaejune Yun <jjune0427@naver.com>
2024-11-01 22:37:21 +0900

    9935번 풀이

--- Test.cpp
+++ Test.cpp
@@ -2,96 +2,45 @@
 
 using namespace std;
 
-//0은 세로 1은 가로
-
-int n, m;
-int num;
-int maxResult = 0;
-vector<vector<int>> v(5);
-vector<vector<int>> bits(5, vector<int>(5, 0));  // 초기화된 크기 지정
-int visited[5][5];
-
-void goGa(int y, int x)
-{
-	visited[y][x] = 1;
-
-	num += v[y][x];
-	if (x + 1 >= m) return;
-	if (visited[y][x + 1]) return;
-
-	if (bits[y][x + 1] == 1) {
-		num *= 10;
-		goGa(y, x + 1);
-	}
-}
-
-void goSe(int y, int x)
-{
-	visited[y][x] = 1;
-
-	num += v[y][x];
-	//cout << "v : " << v[y][x] << "  ";
-	//cout << "num " << num << "\n";
-	if (y + 1 >= n) return;
-	if (visited[y + 1][x]) return;
-
-	if (bits[y + 1][x] == 0) {
-		num *= 10;
-		goSe(y + 1, x);
-	}
-}
-
 int main()
 {
-	cin >> n >> m;
+      ios_base::sync_with_stdio(0);
+      cin.tie(0); cout.tie(0);
 
-	char c;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> c;
-			int num = c - '0';
-			v[i].push_back(num);
-		}
-	}
+      string s, tnt;
+      cin >> s >> tnt;
 
-	for (int t = 0; t < (1 << (n * m)); t++) {  // 모든 경우의 수에 대해 반복
-		//cout << "t :" << t << "\n";
+      deque<char> result;
+      int tntSize = tnt.size();
 
-		// bits를 초기화한 후 인덱스를 사용하여 값을 설정
-		bits.assign(n, vector<int>(m, 0));
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (t & (1 << (i * m + j))) {  // m 대신 n+1을 사용하면 잘못된 비트 위치 참조
-					bits[i][j] = 1;
-				}
-				else {
-					bits[i][j] = 0;
-				}
-			}
-		}
+      for (char ch : s) {
+            result.push_back(ch);               //s에 있는 요소를 순서대로 deque에 넣는다.
 
-		fill(&visited[0][0], &visited[0][0] + 5 * 5, 0);
+            if (result.size() >= tntSize) {
+                  bool match = true;
+                  for (int i = 0; i < tntSize; ++i) {                   //폭발 문자열 크기만큼 돌린다.
+                        if (result[result.size() - tntSize + i] != tnt[i]) {        //result[result의 전체 사이즈 - 폭발 문자열 + i] != tnt[i] -> 즉, 목발문자열의 순서대로 비교하는 것이다. 강의의 erase와 동일한 방법.
+                              match = false;
+                              break;
+                        }
+                  }
 
-		int tempSum = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (visited[i][j]) continue;
+                  if (match) {            //폭발윤자열과 동일하다면
+                        for (int i = 0; i < tntSize; ++i) {
+                              result.pop_back();            //해당 개수만큼 deque에서 뺀다.
+                        }
+                  }
+            }
+      }
 
-				num = 0;
-				//cout << "i : " << i << " j : " << j << "   bits result : " << bits[i][j] << "\n";
-				if (bits[i][j] == 1) {
-					goGa(i, j);
-				}
-				else {
-					goSe(i, j);
-				}
-				//cout << "num : " << num << "\n";
-				tempSum += num;
-			}
-		}
+      if (result.empty()) {
+            cout << "FRULA";
+      }
+      else {
+            for (char ch : result) {
+                  cout << ch;
+            }
+      }
 
-		maxResult = max(maxResult, tempSum);
-	}
-
-	cout << maxResult;
+      return 0;
 }
