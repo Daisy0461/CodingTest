8febefeef31582369d4f84ef886fec509e548db7
Jaejune <jjune0427@naver.com>
2024-11-09 02:09:37 +0900

    13144 완료

--- Coding/Test.cpp
+++ Coding/Test.cpp
@@ -4,78 +4,37 @@ using namespace std;
 
 typedef long long ll;
 
-//연속된 소수의 합으로 n을 만들어낸다.
 int n;
-int result = 0;
+int visited[100001];
 vector<int> v;
+ll result=0;
 
-bool check(int num)
-{
-	if (num == 2 || num == 3) {
-		return true;
-	}
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0); cout.tie(0);
 
-	if (num % 2 == 0) {		//num이 짝수라면 
-		return false;			//소수가 아니다.
-	}
+      cin >> n;
 
-	bool flag = true;
-	for (int i = 3; i * i <= num; i = i + 2) {		
-		if (num % i == 0) {
-			flag = false;
-			break;
-		}
-	}
+      int temp;
+      for (int i = 0; i < n; i++) {
+            cin >> temp;
+            v.push_back(temp);
+      }
 
-	return flag;
+      int point = 0;  // 시작 포인터
+      for (int i = 0; i < n; i++) {
+            while (visited[v[i]]) {  // 중복이 발생하면 point 이동
+                  visited[v[point]] = 0;  // point 요소의 방문 여부를 해제
+                  point++;
+             }
+
+            visited[v[i]] = 1;  // 현재 요소 방문 처리
+
+            // i - point + 1은 point부터 i까지의 중복 없는 부분 수열의 개수
+            result += (i - point + 1);
+      }
+
+      cout << result;
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-	cin >> n;
-
-	if (n == 1) {
-		cout << 0;
-		return 0;
-	}
-	else if (n == 2) {
-		cout << 1;
-		return 0;
-	}
-
-	for (int i = 2; i < n; i++) {
-		if (check(i)) {		//i는 소수이다.
-			v.push_back(i);		//sort 불필요.
-		}
-	}
-
-	/*for (int i : v) {
-		cout << i << " ";
-	}*/
-
-	if (check(n)) {
-		result++;
-	}
-
-	int s = 0;
-	while (v[s] <= n/2) {
-		int sum = 0;
-		for (int i = s; i < n; i++) {
-			sum += v[i];
-			if (sum == n) {
-				result++;
-				s++;
-				break;
-			}
-			else if (sum > n) {
-				s++;
-				break;
-			}
-		}
-	 }
-
-	cout << result;
-}
+//5 1 2 3 2 1
