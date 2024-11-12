891e3a2311a580efe8517bbcbfe3740eb4777b1c
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2024-11-13 02:34:05 +0900

    17411 완료

--- Test.cpp
+++ Test.cpp
@@ -1,53 +1,136 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int n, k, result = 0;
-int visited[101];
-vector<int> v;
-vector<int> cv;
+int r, c, t;
+int room[51][51];
+int add[51][51];
+int dr[4] = { 0, 1, 0, -1 };
+int dc[4] = { -1, 0, 1, 0 };
+int up_row, down_row; // 공기청정기 윗부분과 아랫부분의 행
+int total_dust;       // 총 먼지량
+
+void input()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> r >> c >> t;
+    bool flag = false;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> room[i][j];
+            if (room[i][j] == -1)
+            { 
+                if (!flag)
+                {
+                    up_row = i;
+                    flag = true;
+                }
+                else
+                    down_row = i;
+            }
+            else
+                total_dust += room[i][j];
+        }
+    }
+}
+
+// 먼지의 확산 함수
+void spreadDust()
+{
+    // 미세먼지 확산량 계산
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            int cnt = 0;
+            int val = room[i][j] / 5;
+            if (room[i][j] == 0 || room[i][j] == -1)
+                continue;
+            for (int k = 0; k < 4; k++)
+            {
+                int nr = i + dr[k];
+                int nc = j + dc[k];
+                if (nr < 0 || nr >= r || nc < 0 || nc >= c)
+                    continue;
+                if (room[nr][nc] == -1)
+                    continue;
+                add[nr][nc] += val;
+                cnt++;
+            }
+            add[i][j] -= (cnt * val);
+        }
+    }
+
+    // 미세먼지 확산 업데이트
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            room[i][j] += add[i][j];
+            add[i][j] = 0;
+        }
+    }
+}
+
+// 공기청정기 순환에 의한 먼지 이동
+void airCleaner()
+{
+    // 사라지는 먼지 계산
+    total_dust -= room[up_row - 1][0];
+    total_dust -= room[down_row + 1][0];
+
+    // 위의 공기 순환 (반시계)
+    // 1. 왼쪽줄
+    for (int i = up_row - 1; i > 0; i--)
+        room[i][0] = room[i - 1][0];
+    // 2. 윗줄
+    for (int i = 0; i < c - 1; i++)
+        room[0][i] = room[0][i + 1];
+    // 3. 오른쪽줄
+    for (int i = 1; i <= up_row; i++)
+        room[i - 1][c - 1] = room[i][c - 1];
+    // 4. 아랫줄
+    for (int i = c - 1; i > 1; i--)
+        room[up_row][i] = room[up_row][i - 1];
+    room[up_row][1] = 0;
+
+    // 아래공기 순환 (시계)
+    // 1. 왼쪽줄
+    for (int i = down_row + 1; i < r - 1; i++)
+        room[i][0] = room[i + 1][0];
+    // 2. 아랫줄
+    for (int i = 0; i < c - 1; i++)
+        room[r - 1][i] = room[r - 1][i + 1];
+    // 3. 오른쪽줄
+    for (int i = r - 1; i >= down_row; i--)
+        room[i][c - 1] = room[i - 1][c - 1];
+    // 4. 윗줄
+    for (int i = c - 1; i > 1; i--)
+        room[down_row][i] = room[down_row][i - 1];
+    room[down_row][1] = 0;
+}
+
+void solve()
+{
+    while (t--)
+    {
+        // 미세먼지 확산
+        spreadDust();
+        // 공기순환에 의한 미세먼지 이동
+        airCleaner();
+    }
+    cout << total_dust << '\n';
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    input();
+    solve();
 
-	cin >> n >> k;
-
-	int temp;
-	for (int i = 0; i < k; i++) {
-		cin >> temp;
-		v.push_back(temp);
-	}
-
-	for (int i = 0; i < k; i++) {
-		if (!visited[v[i]]) {		//v[i]가 현재 콘센트에 없다.
-			if (cv.size() == n) {	//현재 콘센트가 꽉 찼다.
-				int last_Index = 0; int pos;
-				for (int t : cv) {
-					int tempIndex = 987654321;
-					for (int j = i + 1; j < k; j++) {
-						if (v[j] == t) {		//이후에 사용할 리스트(v)에서 현재 꽂혀있는 t와 동일한 것을 찾았다.
-							tempIndex = j;		//그 가전제품은 j번째에서 다시 사용한다. 이 사용하는 것은 이후에도 존재할 수 있으나 제일 가까이서 사용하는 것을 찾는다.
-							break;
-						}
-					}
-
-					if (last_Index < tempIndex) {			//지금 찾은 index값이 지금까지 찾았던 Index보다 값이 크다. 즉, 더 이후에 사용한다.
-						last_Index = tempIndex;
-						pos = t;
-					}
-				}
-
-				visited[pos] = 0;
-				result++;
-				cv.erase(find(cv.begin(), cv.end(), pos));
-			}
-
-			visited[v[i]] = 1;
-			cv.push_back(v[i]);
-		}
-	}
-
-	cout << result;
+    return 0;
 }
