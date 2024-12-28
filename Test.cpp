1cf1d252e0f867b04456e2604bbefc97643790fe
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2024-12-29 01:30:21 +0900

    드래곤 커브 성공

--- Test.cpp
+++ Test.cpp
@@ -2,49 +2,32 @@
 
 using namespace std;
 
-int dc, result = 0;
-int sx, sy, sd, g;			//시작 x, y위치, direction(1 = ->, 2 = 아래, 3 = <-, 4= 위), g = 몇번 커브하는가? g = 0일 때 direction 방향으로 한번은 간다.
-int dy[] = { 0, -1, 0 ,1 };
-int dx[] = { 1, 0, -1, 0 };
-
-int sumArray[105][105];
-vector<pair<int, int>> tempPoints;
-
+int dc;
+int result = 0;
+int dy[] = { 0, -1, 0, 1 };
+int dx[] = { 1, 0 ,-1, 0 };
+int points[105][105];
+vector<int> pastDir;
 
 void dragonCurve(int y, int x, int dir, int g)
 {
-	if (g < 0) {
-		tempPoints.push_back({ sy, sx });
-		cout << "tempPoints Size : " << tempPoints.size() << "\n";
-		for (auto i : tempPoints) {
-			cout << "y: " << i.first << "   x : " << i.second << "\n";
-			sumArray[i.first][i.second] = 1;
+	points[y][x] = 1;
+	int ny = y + dy[dir];
+	int nx = x + dx[dir];
+	pastDir.push_back(dir);
+	points[ny][nx] = 1;
+
+	while (g) {
+		g -= 1;
+		int pd = pastDir.size() - 1;
+		for (int i = pd; i >= 0; i--) {		//뒤에서부터 앞으로
+			int nd = (pastDir[i] + 1) % 4;
+			ny = ny + dy[nd];
+			nx = nx + dx[nd];
+			pastDir.push_back(nd);
+			points[ny][nx] = 1;
 		}
-		return;
 	}
-
-	g -= 1;
-	int ny = y, nx = x;
-	if (!tempPoints.size()) {
-		ny = y + dy[dir];
-		nx = x + dx[dir];
-		tempPoints.push_back({ ny, nx });
-		dir = (dir + 1) % 4;
-		dragonCurve(ny, nx, dir, g);
-		return;
-	}
-
-	//최초가 아니다.
-	int ts = tempPoints.size();
-	for (int i = 0; i < ts; i++) {
-		//dir은 이미 갱신 되어있음.
-		ny = ny + dy[dir];
-		nx = nx + dx[dir];
-		tempPoints.push_back({ ny, nx });
-		dir = (dir + 1) % 4;
-	}
-
-	dragonCurve(ny, nx, dir, g);
 
 }
 
@@ -52,29 +35,8 @@ void FindSquere()
 {
 	for (int i = 0; i < 100; i++) {
 		for (int j = 0; j < 100; j++) {
-			bool bIsFind = true;
-			for (int d = 0; d < 4; d++) {
-				if (sumArray[i + dy[d]][j + dx[d]] == 0) {
-					bIsFind = false;
-				}
-			}
-
-			//정사각형 찾음.
-			if (bIsFind) {
+			if (points[i][j] && points[i + 1][j] && points[i][j + 1] && points[i + 1][j + 1]) {
 				result++;
-			}
-		}
-	}
-	
-}
-
-void printPoints()
-{
-	cout << "\n\nFind Points \n";
-	for (int i = 0; i < 101; i++) {
-		for (int j = 0; j < 101; j++) {
-			if (sumArray[i][j] == 1) {
-				cout << "y : " << i << "    x : " << j << "\n";
 			}
 		}
 	}
@@ -86,14 +48,13 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 	cin >> dc;
-	
-	for (int i = 0; i < dc; i++) {
-		tempPoints.clear();
+	int sx, sy, sd, g;
+	for (int i=0; i < dc; i++) {
+		pastDir.clear();
 		cin >> sx >> sy >> sd >> g;
 		dragonCurve(sy, sx, sd, g);
 	}
 
-	printPoints();
 	FindSquere();
 
 	cout << result;
