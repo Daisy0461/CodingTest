865bd422bd759f8be119b7419eae1fae8ed969e0
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-01-30 13:48:03 +0900

    15683 완료

--- Test.cpp
+++ Test.cpp
@@ -2,66 +2,144 @@
 
 using namespace std;
 
-int n, result = -987654321;
-int arr_input[21][21];		//처음 들어오는 Input
+int n, m;
+int result = 987654321;
+int room[10][10];
+int dy[] = { -1, 0 ,1, 0 };
+int dx[] = { 0, 1, 0, -1 };
+vector<pair<int, int>> cctvPos;
 
-deque<int> dq;
-
-void play(int playCount, int inputArray[21][21])
+vector<pair<int, int>>  watch(int cctvIndex, int dir)
 {
-	if (playCount == 5) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				result = max(inputArray[i][j], result);
+	vector<pair<int, int>> changed;
+	int ny = cctvPos[cctvIndex].first;		//첫 위치를 ny, nx에 저장
+	int nx = cctvPos[cctvIndex].second;
+	int cctvNum = room[ny][nx];		//어떤 종류의 CCTV인지 저장.
+
+	//cout << "CCTV Y : " << ny << "  X :" << nx << "\n";			//위치는 잘 나오는데 왜 해당 arr에 들어 있는 값이 0이지?
+	//cout << "cctv Num : " << cctvNum << "\n";
+
+	if (cctvNum == 1) {
+		ny = cctvPos[cctvIndex].first;		//위치를 CCTV위치로 초기화
+		nx = cctvPos[cctvIndex].second;
+		while (true) {
+			ny = ny + dy[dir];
+			nx = nx + dx[dir];
+
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) break;
+			if (room[ny][nx] == 6) break;
+
+			if (room[ny][nx] == 0) {			//이렇게 하지 않으면 CCTV가 0으로 바뀔 수가 있다.
+				changed.push_back({ ny, nx });		//이 위치는 0에서 본 것으로 바뀐 위치이다.
+				room[ny][nx] = 7;
 			}
 		}
+	}
+	else if (cctvNum == 2) {
+		for (int i = 0; i <= 2; i += 2) {
+			ny = cctvPos[cctvIndex].first;		//위치를 CCTV위치로 초기화
+			nx = cctvPos[cctvIndex].second;
+			while (true) {
+				ny = ny + dy[(i + dir) % 4];
+				nx = nx + dx[(i + dir) % 4];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m) break;
+				if (room[ny][nx] == 6) break;
+
+				if (room[ny][nx] == 0) {			//이렇게 하지 않으면 CCTV가 0으로 바뀔 수가 있다.
+					changed.push_back({ ny, nx });		//이 위치는 0에서 본 것으로 바뀐 위치이다.
+					room[ny][nx] = 7;
+				}
+			}
+		}
+	}
+	else if (cctvNum == 3) {
+		for (int i = 0; i < 2; i++) {
+			ny = cctvPos[cctvIndex].first;		//위치를 CCTV위치로 초기화
+			nx = cctvPos[cctvIndex].second;
+			while (true) {
+				ny = ny + dy[(i + dir) % 4];
+				nx = nx + dx[(i + dir) % 4];
+
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m) break;
+				if (room[ny][nx] == 6) break;
+
+				if (room[ny][nx] == 0) {			//이렇게 하지 않으면 CCTV가 0으로 바뀔 수가 있다.
+					changed.push_back({ ny, nx });		//이 위치는 0에서 본 것으로 바뀐 위치이다.
+					room[ny][nx] = 7;
+				}
+			}
+		}
+	}
+	else if (cctvNum == 4) {
+		for (int i = 0; i < 3; i++) {
+			ny = cctvPos[cctvIndex].first;		//위치를 CCTV위치로 초기화
+			nx = cctvPos[cctvIndex].second;
+			while (true) {
+				ny = ny + dy[(i + dir) % 4];
+				nx = nx + dx[(i + dir) % 4];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m) break;
+				if (room[ny][nx] == 6) break;
+
+				if (room[ny][nx] == 0) {			//이렇게 하지 않으면 CCTV가 0으로 바뀔 수가 있다.
+					changed.push_back({ ny, nx });		//이 위치는 0에서 본 것으로 바뀐 위치이다.
+					room[ny][nx] = 7;
+				}
+			}
+		}
+	}
+	else if (cctvNum == 5) {
+		for (int i = 0; i < 4; i++) {
+			ny = cctvPos[cctvIndex].first;		//위치를 CCTV위치로 초기화
+			nx = cctvPos[cctvIndex].second;
+
+			while (true) {
+				ny = ny + dy[(i + dir) % 4];
+				nx = nx + dx[(i + dir) % 4];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m) break;
+				if (room[ny][nx] == 6) break;
+
+				if (room[ny][nx] == 0) {			//이렇게 하지 않으면 CCTV가 0으로 바뀔 수가 있다.
+					changed.push_back({ ny, nx });		//이 위치는 0에서 본 것으로 바뀐 위치이다.
+					room[ny][nx] = 7;
+				}
+			}
+		}
+	}
+
+	return changed;
+}
+
+void check(int checkCount)
+{
+	if (checkCount == cctvPos.size()) {
+		int tempMiss = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (room[i][j] == 0) {
+					tempMiss += 1;
+				}
+			}
+		}
+		//
+		//cout << "\nTempMiss : " << tempMiss << "\n";
+		//for (int i = 0; i < n; i++) {
+		//	for (int j = 0; j < m; j++) {
+		//		cout << room[i][j] << " ";
+		//	}
+		//	cout << "\n";
+		//}
+		//cout << "\n";
+		result = min(result, tempMiss);
 		return;
 	}
 
 	for (int k = 0; k < 4; k++) {
-		int tempArray[21][21];
-		fill(&tempArray[0][0], &tempArray[0][0] + 21 * 21, 0);
-		for (int i = 0; i < n; i++) {
-			dq.push_back(0);
-			for (int j = 0; j < n; j++) {
-				//if (inputArray[i][j] == 0) continue;
-				int back = dq.back();
-				if (inputArray[i][j] == back) {
-					dq.pop_back();
-					dq.push_back(back * 2);
-				  dq.push_back(0);
-				}
-				else {
-					dq.push_back(inputArray[i][j]);
-				}
-			}
-			//가로 한 줄이 끝났다.
-			int pushIndex = 0;
-			while (dq.size()) {
-				if (dq.front() == 0) {		//0은 무시한다.
-					dq.pop_front();
-				}
-				else {
-					tempArray[i][pushIndex] = dq.front();
-					dq.pop_front();
-					pushIndex++;
-				}
-			}
-		}
-		//arr_temp가 2024게임의 왼쪽으로 shift한 모양이 된다.
-		play(playCount + 1, tempArray);
+		vector < pair<int, int>> changedPos = watch(checkCount, k);		//각 CCTV에 대해서 4방향 다 check함.
+		check(checkCount + 1);			//다음 CCTV로 넘어가서 확인함.
 
-		int arr_rotate[21][21];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				arr_rotate[i][j] = inputArray[n-j-1][i];
-			}
-		}
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				inputArray[i][j] = arr_rotate[i][j];
-			}
+		//cout << "Chnaged Pos : " << changedPos.size() << "\n";
+		for (auto i : changedPos) {
+			room[i.first][i.second] = 0;		//다시 보지 못한 것으로 원복
 		}
 	}
 }
@@ -71,18 +149,21 @@ int main()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n;
+	cin >> n >> m;
 
-	//초기 상태 입력
 	int temp;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+		for (int j = 0; j < m; j++) {
 			cin >> temp;
-			arr_input[i][j] = temp;
+			room[i][j] = temp;
+			if (temp != 6 && temp != 0) {
+				cctvPos.push_back({ i,j });
+			}
 		}
 	}
 
-	play(0, arr_input);
+	check(0);
 
+	//cout << "result : ";
 	cout << result;
 }
