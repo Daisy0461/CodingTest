1187c810a33f53cdcf84df6b5eb1da7688a53c0b
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-02-09 03:13:54 +0900

    6236 성공

--- Test.cpp
+++ Test.cpp
@@ -1,56 +1,53 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
 
-int n, result = -1;
-int nums[1001];
-int prevIndex[1001];
-int numStacks[1001];
+int n, m;		//m번만 돈을뺀다.
+int result = 100000 * 10000 + 100;
 vector<int> v;
+
+bool check(int num)
+{
+	int a = 0; int takeMoney = 1;
+	for (int i : v) {
+		a += i;
+		if (i > num) return false;		//이미 하루가 num보다 크다. -> 금액이 맞지 않다.
+
+		if (a > num) {		//총합 한 값이 하루의 금액보다 크다.
+			a = i;
+			takeMoney += 1;
+		}
+	}
+
+	return takeMoney <= m;		//m번만 돈을 빼는데 이 함수 내에서 한 TakeMoney가 더 작거나 같다.
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n;
+	cin >> n >> m;
+
+	int temp;
+	int sum = 0;				//모든 금액 총합
 	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
+		cin >> temp;
+		v.push_back(temp);
+		sum += temp;
 	}
 
-	fill(&prevIndex[0], &prevIndex[0] + 1001, -1);
-	fill(&numStacks[0], &numStacks[0] + 1001, 0);	
-
-	int highestIndex = 0;
-	for (int i = 0; i < n; i++) {
-		int prevIndexNum = i;
-		for (int j = 0; j < i; j++) {
-			if (nums[j] < nums[i] && numStacks[i] < numStacks[j]) {		//현재 i보다 왼쪽에 있는 수가 더 작다. && 그 왼쪽에 있는 수의 Stack이 현재 수의 Stack보다 크다.
-				numStacks[i] = numStacks[j];		//현재 숫자가 이전 숫자의 높이까지는 왔다. -> 계속 오른쪽으로 나아갈 수록 더 높은 Stack이 있으니 그걸로 갱신한다.
-				prevIndexNum = j;		//현재 숫자보다 바로 직전에 높았던 숫자는 nums[j]에 있는 숫자이다.
-			}
+	int left = 1, right = sum, mid;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (check(mid)) {
+			result = min(result, mid);
+			right = mid - 1;
 		}
-
-		prevIndex[i] = prevIndexNum;		//현재 숫자 이전에 가장 높았던 숫자의 Index를 저장. 
-		numStacks[i] = numStacks[i] + 1;
-
-		if (result < numStacks[i]) {		//현재 숫자가 이전까지의 Stacks보다 높다.
-			highestIndex = i;
-			result = numStacks[i];
-			cout << "hightest Index : " << i << "\n";
+		else {		//금액을 더 크게 만든다.
+			left = mid + 1;
 		}
 	}
 
-	int findNumIndex = highestIndex;
-	while (findNumIndex != prevIndex[findNumIndex]) {
-		v.push_back(nums[findNumIndex]);				//findNumIndex가 가리키는 숫자를 넣는다.
-		findNumIndex= prevIndex[findNumIndex];		//findNumIndex를 현재 숫자의 바로 이전 높았던 숫자를 가르키게 한다.
-	}
-	v.push_back(nums[findNumIndex]);		//마지막 추가
-	reverse(v.begin(), v.end());
-
-	cout << "result \n";
-	for (int i : v) {
-		cout << i << " ";
-	}
+	cout << result;
 }
