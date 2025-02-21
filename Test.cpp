4641ef00981a3b28552740f7edd80164dd7a9db9
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-02-22 01:43:25 +0900

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -3,53 +3,32 @@
 using namespace std;
 
 int n;
-vector<int> v;
-vector<int> nums;
-int upResult = 0;
-int prev_arr[1000010];
-int upCounts[1000010];
+int lis[1000010],len = 0, num;
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
+	//fill(&lis[0], &lis[0] + 1000010, 1e9 + 1);
 
 	cin >> n;
-	int temp;
+
 	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		v.push_back(temp);
+		cin >> num;
+		auto lowerPos = lower_bound(lis, lis + len, num);
+		if (!*lowerPos) len++;
+		*lowerPos = num;
+		cout << *lowerPos << "\n";
+		for (int j = 0; j < n; j++) {
+			cout << lis[j] << " ";
+		}
+		cout << "\n";
 	}
-	fill(&prev_arr[0], &prev_arr[0] + 1000010, -1);
-	fill(&upCounts[0], &upCounts[0] + 1000010, 1);
 
 	int index = 0;
-	for (int i = 0; i < n; i++) {			//현재 num = i
-		for (int j = 0; j < i; j++) {			//이전 num = j
-			if (v[i] > v[j] && upCounts[i] <= upCounts[j]) {		//현재 숫자가 이전에 있던 숫자보다 크다.
-				upCounts[i] = upCounts[j] + 1;		//이전(j) 보다 +1한다.
-				prev_arr[i] = j;		//현재 num의 이전 높던 값은 j이다.
-
-				if (upResult < upCounts[i]) {
-					upResult = upCounts[i];
-					index = i;		//마지막 요소를 위해서 저장한다.
-				}
-			}
-		}
+	while (lis[index]) {
+		cout << lis[index] << " ";
+		index += 1;
 	}
-
-	vector<int> results;
-	while (index != -1) {
-		//cout << "push Num : " << v[index] << "\n";
-		results.push_back(v[index]);
-		index = prev_arr[index];
-	}
-
-	reverse(results.begin(), results.end());
-
-	//cout << "results \n";
-	cout << upResult << '\n';
-	for (int i = 0; i < results.size(); i++) {
-		cout << results[i] << " ";
-	}
+	cout << len;
 }
