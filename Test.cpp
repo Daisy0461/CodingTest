59f20d88a89f124e025b8343579d54d03c144973
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-02-22 18:00:48 +0900

    2670  완료

--- Test.cpp
+++ Test.cpp
@@ -2,33 +2,37 @@
 
 using namespace std;
 
-int n;
-int lis[1000010],len = 0, num;
+int n; 
+double num;
+double maxValue = 0;
+vector<double> v;
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	//fill(&lis[0], &lis[0] + 1000010, 1e9 + 1);
 
 	cin >> n;
-
 	for (int i = 0; i < n; i++) {
 		cin >> num;
-		auto lowerPos = lower_bound(lis, lis + len, num);
-		if (!*lowerPos) len++;
-		*lowerPos = num;
-		cout << *lowerPos << "\n";
-		for (int j = 0; j < n; j++) {
-			cout << lis[j] << " ";
-		}
-		cout << "\n";
+		v.push_back(num);
 	}
 
-	int index = 0;
-	while (lis[index]) {
-		cout << lis[index] << " ";
-		index += 1;
+	double value = 1;
+	for (double i : v) {
+		maxValue = max(maxValue, i);		//i 하나가 가장 클 수 있으므로 작성.
+
+		if (value * i < 1) {
+			value = 1;		//지금까지 구한 값이 1보다 작다면 이미 이후에 곱하는 값은 더 작아진다. 이때 max랑 비교하면 안된다.
+		}
+		else {
+			value = value * i;
+			maxValue = max(maxValue, value);
+		}
 	}
-	cout << len;
+
+	cout << fixed;
+	cout.precision(3);
+	cout << maxValue << '\n';
+
 }
