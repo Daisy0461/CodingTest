9626222ec819ebd72f7fdfa98bd01659f6ce04b1
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-02-22 19:19:27 +0900

    11053 완료

--- Test.cpp
+++ Test.cpp
@@ -2,37 +2,23 @@
 
 using namespace std;
 
-int n; 
-double num;
-double maxValue = 0;
-vector<double> v;
+int n, len=0, num;
+int arr[1005];
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
+	fill(&arr[0], &arr[0] + 1005, 0);
 	cin >> n;
+
 	for (int i = 0; i < n; i++) {
 		cin >> num;
-		v.push_back(num);
+		auto lowerPos = lower_bound(arr, arr + len, num);
+		if (*lowerPos == 0) len++;
+		*lowerPos = num;
 	}
 
-	double value = 1;
-	for (double i : v) {
-		maxValue = max(maxValue, i);		//i 하나가 가장 클 수 있으므로 작성.
-
-		if (value * i < 1) {
-			value = 1;		//지금까지 구한 값이 1보다 작다면 이미 이후에 곱하는 값은 더 작아진다. 이때 max랑 비교하면 안된다.
-		}
-		else {
-			value = value * i;
-			maxValue = max(maxValue, value);
-		}
-	}
-
-	cout << fixed;
-	cout.precision(3);
-	cout << maxValue << '\n';
-
+	cout << len;
 }
