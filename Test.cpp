77b6ce6d51af6ae32cb3004b00639b2fe798dfa6
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-02-23 13:39:28 +0900

    2565 완료

--- Test.cpp
+++ Test.cpp
@@ -2,38 +2,32 @@
 
 using namespace std;
 
-int n, num, len = 0;
-int arr[1004];
-pair<int, int > p[1004];
-vector<int> v;
+int n, s, e;
+vector<pair<int, int>> vp;
+int arr[105];
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n;
+	cin >> n;		//총 전기줄의 개수
 	for (int i = 0; i < n; i++) {
-		cin >> num;
-		auto lowerPos = lower_bound(arr, arr + len, num);
-		int pos = (int)(lower_bound(arr, arr + len, num) - arr);
-		if (*lowerPos == 0) len++;
-		*lowerPos = num;
-		p[i].first = num;
-		p[i].second = pos;		//해당 num이 arr배열에서 몇번쨰 index에 추가되었는지를 확인한다.  - 2번에 있던 것에서 갱신되면 2가 들어가는 형식이다.
+		cin >> s >> e;
+		vp.push_back({ s, e });
 	}
 
-	cout << len << '\n';
-	for (int i = n - 1; i >= 0; i--) {
-		if (p[i].second == len - 1) {
-			len -= 1;
-			v.push_back(p[i].first);
-		}
-	}
-	reverse(v.begin(), v.end());
+	sort(vp.begin(), vp.end());
 
+	fill(&arr[0], &arr[0] + 105, 0);
 
-	for (int i : v) {
-		cout << i << " ";
+	int len = 0;
+	for (int i = 0; i < vp.size(); i++) {
+		int t = vp[i].second;
+		auto lowerPos = lower_bound(arr, arr + len, t);
+		if(*lowerPos == 0) len++;
+		*lowerPos = t;
 	}
+
+	cout << n - len;
 }
