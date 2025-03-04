4b8ac2fe6b82b696ae1f89ae937dd0437ae59bfd
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-03-04 17:55:29 +0900

    1로 만들기 Bottom up

--- Test.cpp
+++ Test.cpp
@@ -7,27 +7,6 @@ using namespace std;
 int n;
 int dp[1000010];
 
-int go(int num)
-{
-	//cout << "now num : " << num << "\n";
-	if (num == 1) return 0;
-	if (dp[num] != 0) return dp[num];
-
-	int di3 = 1e9, di2 = 1e9;
-	if (num % 3 == 0) {
-		di3 = go(num / 3) + 1;
-	}
-	if(num % 2 == 0){
-		di2  =  go(num / 2) + 1;
-	}
-	int sub = go(num - 1) + 1;
-
-	int tempMin = min(di3, di2);
-	tempMin = min(tempMin, sub);
-
-	return dp[num] = tempMin;
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(0);
@@ -37,5 +16,27 @@ int main()
 
 	fill(&dp[0], &dp[0] + 1000010, 0);
 
-	cout << go(n);
+	dp[0] = 0;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+
+	for (int i = 4; i <= n; i++) {
+		int di3 = 1e9, di2 = 1e9, sub = 1e9;
+
+		if (i % 3 == 0) {
+			di3 = dp[i / 3] + 1;
+		}
+		if (i % 2 == 0) {
+			di2 = dp[i / 2] + 1;
+		}
+
+		sub = dp[i - 1] + 1;
+
+		int t = min(di3, di2);
+		t = min(t, sub);
+		dp[i] = t;
+	}
+
+	cout << dp[n];
 }
