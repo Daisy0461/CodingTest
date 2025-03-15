1641da195c03a644edc171105d5aec0e25236656
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-03-15 19:49:27 +0900

    15486 성공 - 이후에 한번 더 다시

--- Test.cpp
+++ Test.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-int n, num, len = 0;
-int nums[1005];
-pair<int, int>	tempMax;		// 지금까지의 최대 stack, 
-int dp[1005];
+typedef long long ll;
+
+int n;		//1<=n<=1500000
+pair<int, int> p[1500010];		//걸리는 날짜, 받는 돈
+int dp[1500010];
 
 int main()
 {
@@ -13,25 +14,19 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 	cin >> n;
-	
-	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
-	}
 
+	int result = 0;
+	int cur = 0;
+	int day, cost;
 	for (int i = 0; i < n; i++) {
-		dp[i] = 1;
+		cin >> day >> cost;
+		
+		cur = max(cur, dp[i]);		//이전의 dp[i]의 값들과 현재 dp[i]의 값과 비교한다.
 
-		for (int j = 0; j <= i; j++) {
-			if (nums[i] < nums[j] && dp[i] <= dp[j]) {		//현재 숫자가 이전의 숫자보다 작다. & 
-				dp[i] = dp[j] + 1;
-			}
+		if (i + day < n) {
+			dp[i + day] = max(dp[i + day], cur + cost);			//이전에 언젠가 dp에 넣었던 값 vs 이전 dp들 중 최대값 + 받을 돈이냐
+			result = max(result, dp[i + day]);
 		}
 	}
-
-	int result = 1;
-	for (int i = 0; i < n; i++) {
-		result = max(dp[i], result);
-	}
-
 	cout << result;
 }
