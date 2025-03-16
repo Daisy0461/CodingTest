e4b10b41a44d8a620363af3cf8a8a9e55ecec011
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-03-16 23:39:34 +0900

    1520 푸는 중

--- Test.cpp
+++ Test.cpp
@@ -2,31 +2,87 @@
 
 using namespace std;
 
-typedef long long ll;
+int n, m, result = 0;
+int arr[510][510];
+pair <int, bool> visited[510][510];		//방문 여부, 성공여부
+int dy[] = { -1, 0 ,1, 0 };
+int dx[] = { 0, 1, 0, -1 };		//위, 오른, 아래, 왼
 
-int n;		//1<=n<=1500000
-pair<int, int> p[1500010];		//걸리는 날짜, 받는 돈
-int dp[1500010];
+bool dfs(int y, int x) 
+{
+	if (y == n - 1 && x == m - 1) {		//왼쪽 하단에 도착했다.
+		visited[y][x].second = true;
+		result += 1;
+		return true;
+	}
+
+	bool bIsGo = false;
+
+	int nowH = arr[y][x];
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;		//범위 밖이면 continue;
+		if (nowH < arr[ny][nx]) continue;			//다음 칸이 더 높다면 갈 수 없으니 continue;
+		if (visited[ny][nx].second == true) {		//이미 왼쪽 끝으로 갈 수 있는 길이 있는 곳이라면
+			result += 1;								//result를 하나 넣어준다
+			visited[y][x].second = true;
+			cout << "ny : " << ny << "  nx : " << nx << " is already true\n";
+			bIsGo = true;
+			continue;
+		}
+
+		if (visited[ny][nx].first) {
+			continue;		//이미 성공한 곳이 아닌데 방문했던 곳이라면 가지 마라
+		}
+
+		visited[ny][nx].first = 1;
+		bIsGo = true;
+		if (!visited[y][x].second) {		//아직 y, x에서 길을 못찾았을 때만 dfs를 돌린다. -> 
+			visited[y][x].second = dfs(ny, nx);
+		}
+	}
+
+	//4방향 중 아무것도 못갔다.
+	if (!bIsGo) {
+		return false;
+	}
+}
+
+void Init()
+{
+	for (int i = 0; i < 501; i++) {
+		for (int j = 0; j < 501; j++) {
+			visited[i][j] = { 0, false };
+		}
+	}
+}
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
+	Init();
+	cin >> n >> m;
 
-	cin >> n;
-
-	int result = 0;
-	int cur = 0;
-	int day, cost;
 	for (int i = 0; i < n; i++) {
-		cin >> day >> cost;
-		
-		cur = max(cur, dp[i]);		//이전의 dp[i]의 값들과 현재 dp[i]의 값과 비교한다.
-
-		if (i + day < n) {
-			dp[i + day] = max(dp[i + day], cur + cost);			//이전에 언젠가 dp에 넣었던 값 vs 이전 dp들 중 최대값 + 받을 돈이냐
-			result = max(result, dp[i + day]);
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
 		}
 	}
+
+
+	dfs(0, 0);
+
+	cout << "Print bools \n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << visited[i][j].second << " ";
+		}
+		cout << "\n";
+	}
+
+	cout << "\nResult \n";
 	cout << result;
 }
