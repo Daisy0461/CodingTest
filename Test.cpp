aa774305b6ea90ab7c5389ea3e90fa5cfac75a45
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-03-18 00:19:32 +0900

    11722 다시 풀어서 성공

--- Test.cpp
+++ Test.cpp
@@ -2,48 +2,32 @@
 
 using namespace std;
 
-int n, m;
-int arr[501][501];
-int dp[501][501];
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, 1, 0, -1 };
-
-int dfs(int y, int x)
-{
-	if (y == n - 1 && x == m- 1) return 1;
-	if (dp[y][x] != -1) return dp[y][x];
-
-	int& temp = dp[y][x];
-	temp = 0;
-
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-		if (arr[y][x] <= arr[ny][nx]) continue;
-
-		temp += dfs(ny, nx);
-	}
-
-	return temp;
-}
+int n;		//1<=n <= 1000
+int result = 0;
+int arr[1010];
+int dp[1010];
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> n >> m;
-
-	fill(&dp[0][0], &dp[0][0] + 501 * 501, -1);
+	cin >> n;
 
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-		}
+		cin >> arr[i];
 	}
 
-	cout << dfs(0, 0);
+	for (int i = 0; i < n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j] && dp[i] <= dp[j]) {		//현재 숫자보다 j가 크면서 쌓인게 더 크다면
+				dp[i] = dp[j] + 1;
+			}
+		}
 
+		result = max(result, dp[i]);
+	}
+	
+	cout << result;
 }
