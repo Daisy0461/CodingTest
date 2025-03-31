c4bdec613c2ddfe6a3d4412fdcc7a92ebc6a8911
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-03-31 23:57:07 +0900

    2156 완료

--- Test.cpp
+++ Test.cpp
@@ -1,36 +1,25 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef long long ll;
 
-//길이가 n인 계단수 ->  n=2인 경우 10,12,21,23,32,34,43,45,54,56 // 65,67,78,76,87,89,98 // 1의 경우 1,2,3,4,5,6,7,8,9 이런 느낌 ㅇㅇ
-
 int n;
-int divNum = 1e9;
-int dp[110][10];
+int arr[10010];
+int dp[10010];
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+int main() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0); cout.tie(0);
 
-	cin >> n;
-	dp[1][0] = 0;
-	for (int i = 1; i <= 9; i++) {
-		dp[1][i] = 1;
-	}
+      cin >> n;
+      for (int i = 1; i <= n; i++) {
+            cin >> arr[i];
+      }
 
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (j - 1 >= 0) dp[i + 1][j - 1] = (dp[i + 1][j - 1] + dp[i][j]) % divNum;
-			if (j + 1 <= 9) dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j]) % divNum;
-		}
-	}
+      dp[1] = arr[1];
+      dp[2] = arr[1] + arr[2];
+      for (int i = 3; i <= n; i++) {
+            dp[i] = max({ dp[i - 1], dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i] });
+      }
 
-	ll result = 0;
-	for (int i = 0; i <= 9; i++) {
-		result = (result + dp[n][i]) % divNum;
-	}
-
-	cout << result;
+      cout << dp[n];
 }
