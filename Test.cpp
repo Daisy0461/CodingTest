7405c12724d230ade7b83a8f01a7fd3018fa3ba3
Daisy0461 <81971201+Daisy0461@users.noreply.github.com>
2025-05-08 14:29:14 +0900

    12865 완료

--- Test.cpp
+++ Test.cpp
@@ -2,70 +2,52 @@
 
 using namespace std;
 
-int testCase;
-int n, fileSize;
-int files[510];
-int sum[510];
-int dp[510][510];
+int n, k;
+int dp[100010];		//해당 무게가 남았을 때 최대 Value
+vector<pair<int, int>> stufs;		//무게, 가치
 
 int main()
-{ 
+{
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	cin >> testCase;
+	cin >> n >> k;
 
-	while (testCase--)
-	{
-		//배열 초기화
-		fill(&files[0], &files[0] + 510, 0);
-		fill(&sum[0], &sum[0] + 510, 0);
-		//fill(&dp[0][0], &dp[0][0] + 510 * 510, 0);
-
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> files[i];
-			//지속 합-> sum[i]는 i까지의 모든 file의 크기 합. 이후에 사용한다.
-			sum[i] = sum[i - 1] + files[i];		
-		}
-
-		for (int indexCount = 1; indexCount < n; indexCount++) {
-
-			//start + indexCount지점까지 -> start부터 indexCount개까지의 합
-			for (int start = 1; indexCount + start <= n; start++) {
-				int end = start + indexCount;
-
-				//미리 구할 범위의 dp를 INT_MAX로 해서 최소값을 찾는다.
-				dp[start][end] = INT_MAX;
-
-				for (int mid = start; mid < end; mid++) {
-					//dp의 의미가 start지점부터 index지점까지의 합의 최소를 구하는 것이다.
-					/*
-					* dp[start][index] = min(기존의 dp[start][index],
-					* dp[start][mid] + dp[mid+1][end]를 통해 mid로 중간을 잘라서 양쪽 값에서 최소를 구한다.
-					* 그렇다면 왜 mid로 구하는가? -> 우린 start부터 end까지의 조합이 어떤 값이 가장 작은지 모른다.
-					* 
-					* 그럼 이미 비교당할 dp의 조합은 어떻게 구하는가?
-					* 이전 for문을 돌면 start가 1부터 늘어날 것이기에 작은 start부터 최소값을찾게 된다.
-					* start부터 indexCount는 1부터 늘어남으로 1~2, 2~3, 3~4처럼 작은 값부터 찾고 그 이후 1~3, 2~4이런 식으로 범위가 늘어가는 구조이다.
-					* 작은 값이 구해져있으면 위에서 설명한 이유로 큰 부분의 최소값을 구할 수 있다.
-					* 
-					* 마지막 sum[end]-sum[start-1]는 파일이 만들어지면 해당 파일의 크기만큼 비용을 쓰게 된다.
-					* 그렇기에 start-1부터 end까지의 범위의 비용을 더해야하는 의미이다.
-					* 처음에 start-1이 아닌 start를 했는데 이는 자기 자신도 빼는 것이라서 start-1을 해야 정확한 범위의 값을 더한다.
-					* 
-					*/
-					//cout << "Compare dp[start][end] : " << dp[start][end] << "  vs calculation : " << dp[start][mid] + dp[mid + 1][end] + (sum[end] - sum[start-1]) << "\n";
-					dp[start][end] = min(dp[start][end], dp[start][mid] + dp[mid + 1][end] + (sum[end] - sum[start-1]));
-					//cout << "dp[" << start << "][" << end << "] is : " << dp[start][end] << "\n";
-				}
-
-				
-			}
-				
-		}
-
-		//cout << "\n result :";
-		cout << dp[1][n] << "\n";
+	int w, v;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> v;
+		stufs.push_back({ w, v });
 	}
+
+	int result = 0;
+	for (int i = 0; i < n; i++) {
+		int nowWegiht = stufs[i].first;
+		int nowValue = stufs[i].second;
+		int nowLeftWegiht = k - nowWegiht;
+		if (nowLeftWegiht < 0) continue;		//이 하나의 물건의 무게가 감당안된다.
+
+		//내가 총 감당 가능한 무게에서 뺀 값을 dp에 넣는다. 이미 있는 값이 크면 따로 넣지 않는다.
+		for (int j = nowWegiht; j < k; j++) {
+			// 어떤 물건을 넣었을 때 현재 물건을 넣을 수 있는 만큼의 무게가 남아있다. &&
+			//원래 j에서 nowWeight를 더한 만큼 남아있는 물건 무게에 다른 물건이 있을 수 있으니
+			//무엇이 더 가치있는지 체크한다.
+			if (dp[j] != 0 && dp[j - nowWegiht] < dp[j] + nowValue) {
+				//cout << "now Weight : " << nowWegiht << "   in \n";
+				//cout << "dp + result =" << dp[j] + nowValue << "\n";
+				dp[j - nowWegiht] = dp[j] + nowValue;
+			}
+		}
+
+		//이 코드가 위에있지 말고 아래이 있어야한다.
+		//그 이후는 현재 무게가 k/2 이하일 때 해당 값이 2번 계산되기 때문이다.
+		if (dp[nowLeftWegiht] < nowValue) {
+			//nowLeftWeight에서 현재까지의 최대값은 nowValue이다.
+			dp[nowLeftWegiht] = nowValue;
+		}
+	}
+
+	for (int i = 0; i < k + 1; i++) {
+		result = max(result, dp[i]);
+	}
+	cout << result;
 }
